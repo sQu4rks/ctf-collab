bfb3b104f3d8eb6d301ea9bad622f7b66885d9a6
Marcel Neidinger (mneiding) <mneiding@cisco.com>
2021-03-09 00:34:02 +0100

    Initial

---
+++ env.py
@@ -0,0 +1,6 @@
+config = {}
+
+config['WEBEX_BASE_URL'] = "https://webexapis.com"
+config['WEBEX_ACCESS_TOKEN'] = "<Replace me in setup>"
+config['PRODUCTION_ROOM'] = "<Replace me in stage 0>"
+config['TESTING_ROOM'] = "<Replace me in stage 1>"
---
+++ verify.py
@@ -0,0 +1,18 @@
+import requests
+
+from env import config
+
+s = requests.Session()
+s.headers.update({
+    'Authorization': f"Bearer {config['token']}"
+})
+
+# Verify api access
+WEBEX_BASE_URL = config['WEBEX_BASE_URL']
+
+url = f"{WEBEX_BASE_URL}/v1/people/me"
+
+resp = s.get(url)
+
+if resp.status_code == 200:
+    print("Webex Access verified")
